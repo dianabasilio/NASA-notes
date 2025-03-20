ac54bf29e4a775da14c0b752fc0ac42210ce3eb6
DIANA KAREN BASILIO BELTRAN <dbasilio@100-dbasilio>
2025-03-19 18:36:19 -0600

    Added fav button.

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import styles from "./Post.module.scss";
 
 function Post({ author, body, urlImage }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <li className={styles.post}>
       <img src={urlImage} alt={urlImage} />
       <p className={styles.author}>{author}</p>
       <p className={styles.text}>{body}</p>
+      <button className={styles.likeButton} onClick={handleLike}>
+        {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+      </button>
     </li>
   );
 }
